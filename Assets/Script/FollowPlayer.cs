07c8428f4ea288d2727e5124f05b27637c1e8a28
KazukiHozumi <hozumikazuki.8140@gmail.com>
2019-02-10 01:21:36 +0900

    Offsetを設定

--- Assets/Script/FollowPlayer.cs
+++ Assets/Script/FollowPlayer.cs
@@ -5,10 +5,16 @@ using System.Collections;
 public class FollowPlayer : MonoBehaviour
 {
     public Transform target;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = GetComponent<Transform>().position - target.position;
+    }
 
     void Update() 
     {
-        GetComponent<Transform>().position = target.position;        
+        GetComponent<Transform>().position = target.position + offset;        
     }
 
 }
