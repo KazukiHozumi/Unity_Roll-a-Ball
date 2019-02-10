670460ba0b6278d9a826d63e3b0aae3a7a7af508
KazukiHozumi <hozumikazuki.8140@gmail.com>
2019-02-11 00:04:15 +0900

    接触判定時にItemを削除

--- Assets/Script/Item.cs
+++ Assets/Script/Item.cs
@@ -6,7 +6,7 @@ public class Item : MonoBehaviour
     private void OnTriggerEnter(Collider hit)
     {
         if(hit.CompareTag("Player")) {
-            
+            Destroy(gameObject);
         }
     }
 }
