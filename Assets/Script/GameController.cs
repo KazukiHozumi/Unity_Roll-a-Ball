12fea14362fc81a1aa9721aa483dc5381c90fe74
KazukiHozumi <hozumikazuki.8140@gmail.com>
2019-02-11 15:02:41 +0900

    Itemをカウントする機能を追加

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public void Update()
+    {
+        int count = GameObject.FindGameObjectWithTag("Item").Length;
+    }
+}
